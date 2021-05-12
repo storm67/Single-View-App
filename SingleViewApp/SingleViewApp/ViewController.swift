a0c2ee6ef692031eb9edad7f02a4cab6e13a9f47
storm67 <64145678+storm67@users.noreply.github.com>
2021-05-12 21:12:46 +0300

    New

---
+++ SingleViewApp/SingleViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by Storm67 on 12/05/2021.
+//  Copyright © 2021 Storm67. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
